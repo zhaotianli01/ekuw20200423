c0da6b3222c948f23f3a62303f4aa5486a532b69
åzhaotianli01 <1095987345@qq.com>
2020-04-23 11:37:25 +0800

    first

---
+++ src/main/java/com/ekuw/EkuwApplication.java
@@ -0,0 +1,11 @@
+package com.ekuw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkuwApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EkuwApplication.class, args);
+    }
+}
